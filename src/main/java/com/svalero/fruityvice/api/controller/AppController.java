61c4fb8a1ab8588cf2dd42eacb77f7062761dce5
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-04-06 18:40:03 +0200

    Build Faile

--- src/main/java/com/svalero/fruityvice/api/controller/AppController.java
+++ src/main/java/com/svalero/fruityvice/api/controller/AppController.java
@@ -1,4 +1,38 @@
 package com.svalero.fruityvice.api.controller;
 
+import com.svalero.fruityvice.api.model.FruitInformation;
+import io.reactivex.functions.Consumer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * declaramos los componentes que tenemos en el fmxl (scene builder)
+ */
 public class AppController {
-}
+
+    @FXML
+    private Button btListAll;
+    @FXML
+    private TextArea listAllArea;
+
+    private List<String> fruitInformations; //Para guardar las datos recibidos de la API en este caso son definiciones de palabras
+
+    /**
+     * Método que se realizara al pulsar el boton en el entorno gráfico
+     * esta definido en el onAction
+     */
+    @FXML
+    public void showAll(ActionEvent event) {
+        this.fruitInformations = new ArrayList<>(); //creamos una lista nueva cada vez que pulsamos el botón buscar que invoca este método
+
+        Consumer<FruitInformation> user = (fruitInformation -> {
+            listAllArea.setText(listAllArea.getText() + "\n" + fruitInformation.getName() + "\n" + fruitInformation.getFamily()); //lo mostramos en el text Area
+            this.fruitInformations.add(fruitInformation.getName() + fruitInformation.getFamily());
+        });
+    }
+ }
